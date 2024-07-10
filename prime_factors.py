39631b7bc4dad87de1066e01f6accf7350b8b6e4
yoon-j <jiyoon.jeon258@gmail.com>
2024-07-10 09:05:45 +0900

    [refactoring] page 33

--- prime_factors.py
+++ prime_factors.py
@@ -1,8 +1,6 @@
 class PrimeFactor:
     def of(self, number) -> []:
         prime_factors = []
-        if number == 2:
-            prime_factors.append(2)
-        if number == 3:
-            prime_factors.append(3)
+        if number > 1:
+            prime_factors.append(number)
         return prime_factors
