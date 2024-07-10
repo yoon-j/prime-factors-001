2afa16c4b69cb146b4e818fb29df20be7a30ccfa
yoon-j <jiyoon.jeon258@gmail.com>
2024-07-10 09:03:22 +0900

    [refactoring] page 27

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,14 +4,15 @@ from prime_factors import PrimeFactor
 
 class TestPrimeFactor(TestCase):
 
+    def setUp(self):
+        super().setUp()
+        self.prime_factor = PrimeFactor()
+
     def test_create_prime_factor(self):
-        prime_factor = PrimeFactor()
-        self.assertIsNotNone(prime_factor)
+        self.assertIsNotNone(self.prime_factor)
 
     def test_prime_factor_of_1(self):
-        prime_factor = PrimeFactor()
-        self.assertEqual(prime_factor.of(1), [])
+        self.assertEqual(self.prime_factor.of(1), [])
 
     def test_prime_factor_of_2(self):
-        prime_factor = PrimeFactor()
-        self.assertEqual(prime_factor.of(2), [2])
+        self.assertEqual(self.prime_factor.of(2), [2])
